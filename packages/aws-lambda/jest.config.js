938568a3f792e41050961561893c1e4f7b52d18e
edwin <edwin.luijten@live.nl>
2023-02-28 16:53:04 +0100

    test: working on code coverage

--- packages/aws-lambda/jest.config.js
+++ packages/aws-lambda/jest.config.js
@@ -2,5 +2,13 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    moduleDirectories: ['node_modules', 'src']
+    collectCoverage: true,
+    coverageDirectory: ".coverage",
+    collectCoverageFrom: ["./src/**", "!./src/index.ts"],
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {tsconfig: './tsconfig.test.json'},
+        ],
+    },
 };
